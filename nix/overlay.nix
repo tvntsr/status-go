a38f8ba04fba6975d7c722298bb053e0e9c465aa
Alexis Pentori <alexis@status.im>
2023-11-23 15:55:13 +0100

    nix: Setting nix directory
    Signed-off-by: Alexis Pentori <alexis@status.im>

---
+++ nix/overlay.nix
@@ -0,0 +1,28 @@
+# Override some packages and utilities in 'pkgs'
+# and make them available globally via callPackage.
+#
+# For more details see:
+# - https://nixos.wiki/wiki/Overlays
+# - https://nixos.org/nixos/nix-pills/callpackage-design-pattern.html
+
+self: super:
+
+let
+  /* FIXME not sure if the stdenv and other are usefull*/
+  inherit (super) stdenv stdenvNoCC callPackage;
+in {
+  /* FIXME not sure for the pkgs*/
+  androidPkgs = pkgs.androidenv.composeAndroidPackages {
+    toolsVersion = "26.1.1";
+    platformToolsVersion = "33.0.3";
+    buildToolsVersions = [ "31.0.0" ];
+    platformVersions = [ "31" ];
+    cmakeVersions = [ "3.18.1" ];
+    ndkVersion = "22.1.7171670";
+    includeNDK = true;
+    includeExtras = [
+      "extras;android;m2repository"
+      "extras;google;m2repository"
+    ];
+  };
+}
