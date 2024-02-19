bd42c6a108fbe86dd213a3f6e7d127c82f11c143
Alexis Pentori <alexis@status.im>
2024-02-19 12:46:50 +0100

    nix: issue located
    Signed-off-by: Alexis Pentori <alexis@status.im>

--- nix/shell.nix
+++ nix/shell.nix
@@ -3,7 +3,6 @@
 
 let
   inherit (pkgs) lib stdenv;
-
   /* No Android SDK for Darwin aarch64. */
   isMacM1 = stdenv.isDarwin && stdenv.isAarch64;
   /* Lock requires Xcode verison. */
@@ -12,6 +11,7 @@ let
     allowHigher = true;
   };
   /* Gomobile also needs the Xcode wrapper. */
+  #FIXME:something is not working with this
   gomobileMod = pkgs.gomobile.override {
     inherit xcodeWrapper;
     withAndroidPkgs = !isMacM1;
@@ -24,12 +24,11 @@ in pkgs.mkShell {
     go_1_19 golangci-lint go-junit-report gopls go-bindata gomobileMod
     mockgen protobuf3_20 protoc-gen-go gotestsum
   ] ++ lib.optional stdenv.isDarwin xcodeWrapper;
-
-  shellHook = lib.optionalString (!isMacM1) ''
-    ANDROID_HOME=${pkgs.androidPkgs.androidsdk}/libexec/android-sdk
-    ANDROID_NDK=$ANDROID_HOME/ndk-bundle
-    ANDROID_SDK_ROOT=$ANDROID_HOME
-    ANDROID_NDK_HOME=$ANDROID_NDK
+   # For some reason `ANDROID_HOME=${pkgs.androidPkgs.androidsdk}/libexec/android-sdk` is not working
+   shellHook = lib.optionalString (!isMacM1) ''
+     ANDROID_NDK=$ANDROID_HOME/ndk-bundle
+     ANDROID_SDK_ROOT=$ANDROID_HOME
+     ANDROID_NDK_HOME=$ANDROID_NDK
   '';
   # Sandbox causes Xcode issues on MacOS. Requires sandbox=relaxed.
   # https://github.com/status-im/status-mobile/pull/13912
